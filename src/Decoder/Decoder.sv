fe9d0df7eedf37626feb94a0862c42833c0261e2
eskilrl <eskilrl@yahoo.com>
2024-10-08 11:54:28 +0200

    Added new decoder design with pure logic gates

--- src/Decoder/Decoder.sv
+++ src/Decoder/Decoder.sv
@@ -1,40 +1,20 @@
 module Decoder (
 input[2:0] i_k_address,
+input valid,
 
 //OUTPUTS
 
 output[7:0] o_select
 );
 
-typedef enum logic[3:0] {
-  cell_0,
-  cell_1,
-  cell_2,
-  cell_3,
-  cell_4,
-  cell_5,
-  cell_6,
-  cell_7
-} e_output_logic;
+assign o_select[0] = !i_k_address[0]&&!i_k_address[1]&&!i_k_address[2]&&valid;
+assign o_select[1] = !i_k_address[0]&&!i_k_address[1]&&i_k_address[2]&&valid;
+assign o_select[2] = !i_k_address[0]&&i_k_address[1]&&!i_k_address[2]&&valid;
+assign o_select[3] = !i_k_address[0]&&i_k_address[1]&&i_k_address[2]&&valid;
+assign o_select[4] = i_k_address[0]&&!i_k_address[1]&&!i_k_address[2]&&valid;
+assign o_select[5] = i_k_address[0]&&!i_k_address[1]&&i_k_address[2]&&valid;
+assign o_select[6] = i_k_address[0]&&i_k_address[1]&&!i_k_address[2]&&valid;
+assign o_select[7] = i_k_address[0]&&i_k_address[1]&&i_k_address[2]&&valid;
 
-e_output_logic addr_select;
-
-always_comb begin
-
-addr_select = i_k_address;
-
-case(addr_select)
-  cell_0: o_select = 8'b00000001;
-  cell_1: o_select = 8'b00000010;
-  cell_2: o_select = 8'b00000100;
-  cell_3: o_select = 8'b00001000;
-  cell_4: o_select = 8'b00010000;
-  cell_5: o_select = 8'b00100000;
-  cell_6: o_select = 8'b01000000;
-  cell_7: o_select = 8'b10000000;
-  default: o_select = 8'b00000000; 
-endcase;
-	
-end;
 
 endmodule
