eedfb34726bf70b1b1b12d7d3fd85f2a8159d38f
Eskilrl <eskilrl@yahoo.com>
2024-10-14 17:59:25 +0200

    Made flip flop

--- src/FlipFlop/FlipFlop.sv
+++ src/FlipFlop/FlipFlop.sv
@@ -8,13 +8,21 @@ input i_wr,
 
 //OUPUTS
 
-ouptut o_outp
+output o_outp
 );
 
+wire w_NOR_out_1;
+wire w_NOR_out_2;
+wire w_NOR_inn_1;
+wire w_NOR_inn_2;
 
 
+assign w_NOR_inn_1 = i_inp&&i_sel&&i_wr;
+assign w_NOR_inn_2 = !i_inp&&i_sel%%i_wr;
 
+assign w_NOR_out_1 = !(w_NOR_inn_1||w_NOR_out_2);
+assign w_NOR_out_2 = !(w_NOR_inn_2||w_NOR_out_1);
 
-
+assign o_outp = !i_wr&&i_sel&&w_NOR_out_2;
 
 endmodule
