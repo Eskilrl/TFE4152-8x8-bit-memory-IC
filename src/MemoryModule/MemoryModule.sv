ed055fdb2b339a132b78ff2813932595218e60c9
Eskilrl <eskilrl@yahoo.com>
2024-10-14 18:36:59 +0200

    Added memory module

---
+++ src/MemoryModule/MemoryModule.sv
@@ -0,0 +1,44 @@
+`import "../MemCell/MemCell.sv"
+`import "../Decoder/Decoder.sv"
+
+module MemoryModule (
+
+//inputs
+  input i0,
+  input i1,
+  input i2,
+  input i3,
+  input i4,
+  input i5,
+  input i6,
+  input i7,
+
+//Adress select
+  input adr0,
+  input adr1,
+  input adr2,
+
+//controll signals
+  input op,
+  input select,
+
+//Outputs
+  output o0,
+  output o1,
+  output o2,
+  output o3,
+  output o4,
+  output o5,
+  output o6,
+  output o7,
+);
+
+
+
+
+
+
+
+
+  
+endmodule
