b1d12010bac76bef7d8674d02e48e8d666db8cea
Eskilrl <eskilrl@yahoo.com>
2024-11-12 11:55:48 +0100

    Reworked FSM logic to use NAND gates

--- src/BitCell/BitCell.sv
+++ src/BitCell/BitCell.sv
@@ -2,34 +2,34 @@ module FlipFlop (
 
 //Inputs
 
-input i_sel,
-input i_inp,
-input i_wr,
+input sel,
+input inp,
+input wr,
 
 //OUPUTS
 
-output o_outp
+output outp
 );
 
-wire w_NAND_out_1;
-wire w_NAND_out_2;
-wire w_NAND_out_3;
-wire w_NAND_out_4;
-wire w_not_rw;
+wire nottempoutp;
+wire tempoutp;
+wire tempinp1;
+wire tempinp2;
+wire notrw;
 
 //inverter
-not(w_not_rw, i_wr);
+not(notrw, wr);
 
 //First layer of nand gates
-nand(w_NAND_out_3, i_inp, i_sel, i_wr);
-nand(w_NAND_out_4, w_NAND_out_3, i_sel, i_wr);
+nand(tempinp1, inp, sel, wr);
+nand(tempinp2, tempinp1, sel, wr);
 
 //second layer of nand gates
-nand(w_NAND_out_2, w_NAND_out_3, w_NAND_out_1);
-nand(w_NAND_out_1, w_NAND_out_4, w_NAND_out_2);
+nand(tempoutp, tempinp1, nottempoutp);
+nand(nottempoutp, tempinp2, tempoutp);
 
 //Define output
-and(o_outp,i_sel,w_not_rw, w_NAND_out_2);
+and(outp,sel,notrw, tempoutp);
 
 
 endmodule
